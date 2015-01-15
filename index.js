e1fa92ba3fd40433ce6a2f5ba037bc6c02260bf7
404pnf <404pnf@exmaple.org>
2015-01-15 14:53:45 +0800

    fix jslint warnings

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+/*global _,*/
+/*jslint browser: true, node: true, nomen: true, */
+'use strict';
+
 var express = require('express'),
-    logger = require('morgan'),
-    app = express(),
-    low = require('lowdb'),
-    _ = require('lodash'),
-    db = low('db.json'),
-    records = db.object,
-    multer  = require('multer');
+  logger = require('morgan'),
+  app = express(),
+  low = require('lowdb'),
+  _ = require('lodash'),
+  db = low('db.json'),
+  records = db.object,
+  multer  = require('multer');
 
 // log requests
 app.use(logger('dev'));
@@ -17,7 +21,7 @@ app.use(multer({
   // rename: function (fieldname, filename) {
   //     return filename + '_' + Date.now(); // 防止相同文件名覆盖之前的上传
   //   }
-  }));
+}));
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/mp3'));
@@ -31,22 +35,22 @@ app.engine('.html', require('ejs').__express);
 app.set('view engine', 'html');
 
 // render
-app.get('/archive', function(req, res) {
+app.get('/archive', function (req, res) {
   res.render('archive', {db: records});
 });
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render('add_song');
 });
 
 
-app.post('/', function(req, res) {
+app.post('/', function (req, res) {
   var body = req.body,
     msg = _.assign(body,
-      {date: (new Date()).toISOString().slice(0,10)},
-      {mp3url: req.files.mp3.name }, // 这个名字是个uuid
-      {originalFilename: req.files.mp3.originalname}
-    );
+        {date: (new Date()).toISOString().slice(0, 10)},
+        {mp3url: req.files.mp3.name },
+        {originalFilename: req.files.mp3.originalname}
+      );
   // console.log(req.body)
   // console.log(req.files)
   // console.log(msg);
